1419c75a8eac9d9ce9d3117bfe81bca0acb5bf32
Lucas Villa Verde <lucasgabrielvverde@gmail.com>
2020-01-30 14:12:02 -0300

    First Commit
    First Commit

---
+++ CSharpTest/Models/Printer.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpTest.Models
+{
+    public class Printer
+    {
+        public string PrinterName { get; set; }
+        public string SerialKey { get; set; }
+        public DateTime? ExpireDate { get; set; }
+        public string CustomerName { get; set; }
+    }
+}
